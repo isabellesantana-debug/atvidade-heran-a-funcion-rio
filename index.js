0fb584b90a78e27709a7de4f43d0880009cd314f
isabelle <isabelle.santana@estudante.ifms.edu.br>
2026-08-12 11:36:36 -0400

    primeiro commit

---
+++ index.js
@@ -0,0 +1,39 @@
+class Funcionario {
+    constructor(nome,salario)
+    this.nome
+    this.salario
+}
+trabalhar()
+{
+        console.log(`${this.nome} ${this.salario} Funcionario esta trabalhando.`);
+    }
+    
+class Funcionario extends Professor 
+
+    constructor(, salario, ) {
+        super(nome, salario,disciplina);
+        this.displina = disciplina;
+    }
+   
+carregar(darAula) {
+        if (disciplina <= 0) {
+            console.log("Elaine esta dando aula.");
+            return;
+        }
+
+        if (disciplina > 2.500) {
+            console.log("Elaine nao esta dando aula");
+            return;
+    }
+     this.disciplina = disciplina;
+        console.log(`O ${this.nome} ${this.salario} agora tem ${this.disciplina}`);
+    }
+
+const Professor = new Professor("Elaine", "R$2.500, Matematica");
+
+console.log(Professor1.nome);
+console.log(Professor1.salario);
+console.log(Professor1.disciplina);
+
+Professor1.trabalhar();
+Professor1.darAula();
